7669d943f90d610e5055a9b4d054fb9651e47f56
halfhoney2018 <halfhoney2018@gmail.com>
2025-09-28 17:02:43 +0800

    feat:初始化项目

---
+++ src/main/java/com/achance/gateway/achinnergateway/AchInnerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.achance.gateway.achinnergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchInnerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchInnerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/achance/gateway/achinnergateway/AchInnerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achance.gateway.achinnergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchInnerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
